d3013309b77a38161637ebb67e754ec61ee1889b
lonelyflame <maks.chursin@gmail.com>
2014-04-26 19:00:37 +0700

    qwe

--- world.gd
+++ world.gd
@@ -14,7 +14,6 @@ var dead = false
 var score = 0
 
 func _ready():
-	set_fixed_process(true)
 	get_node(play_button).connect("pressed",self,"_on_button_pressed")
 
 func _on_button_pressed():
@@ -34,6 +33,7 @@ func reset_obstacle():
 
 
 func start():
+	set_fixed_process(true)
 	get_node(play_button).set_disabled(true)
 	get_node(play_button).set_opacity(0)
 	screen_size = get_viewport_rect().size
@@ -52,6 +52,7 @@ func player_crashed():
 	get_node(play_button).set_disabled(false)
 	get_node(play_button).set_opacity(1)
 	
+	
 func _fixed_process(delta):
 	if !dead:
 
@@ -61,7 +62,7 @@ func _fixed_process(delta):
 		#if (check_intersect(get_node(player), get_node(Obstacle))):
 		#	player_crashed()
 		
-		if obs_bot_pos.y <= player_bot_pos.y-200:
+		if obs_bot_pos.y <= player_bot_pos.y-300:
 			reset_obstacle()
 
 func _on_Area2D_body_enter( body ):
